06c131c880b89bc1c51f4b1023bfddb799218567
ketchadapron <ketchadapron.s@gmail.com>
2024-02-10 21:31:44 -0600

    Created  function nextSequence

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
-alert("h1");
+buttonColors = ["red", "blue", "green", "yellow"];
+gamePattern = [];
+
+// Create function nextSequence for random number amd match with array of colors
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push[randomChosenColor];
+  //   console.log(randomChosenColor);
+  //   console.log(randomNumber);
+}
